dc05d8c49e273fff42e98e7e2770fb9bd28c0408
JeKachan <jekachan7@gmail.com>
2022-05-21 13:15:23 +0300

    Fix indentation in dependencies.ts

--- dependencies.ts
+++ dependencies.ts
@@ -1,9 +1,9 @@
 import {AuthorService, BookService, LogService} from './services/types';
 
 type DependenciesMap = {
-    author: AuthorService
-    book: BookService
-    log: LogService
+	author: AuthorService
+	book: BookService
+	log: LogService
 }
 
 type DependenciesNames = keyof DependenciesMap
@@ -14,32 +14,32 @@ let dependenciesMap: DependenciesMap | undefined
  * @description register all dependencies
  */
 export function registerAll(deps: Readonly<DependenciesMap>) {
-    dependenciesMap = deps
+	dependenciesMap = deps
 }
 
 /**
  * @description register single dependency by name
  */
 export function register<Key extends DependenciesNames>(name: Key, deps: DependenciesMap[Key]) {
-    assertDependenciesMapExists(dependenciesMap)
-    dependenciesMap[name] = deps
+	assertDependenciesMapExists(dependenciesMap)
+	dependenciesMap[name] = deps
 }
 
 /**
  * @description resolve dependency by name
  */
 export function resolve<ServiceName extends DependenciesNames>(name: ServiceName): DependenciesMap[ServiceName] {
-    assertDependenciesMapExists(dependenciesMap)
-    const service = dependenciesMap[name]
-    if (!service) {
-        throw new Error(name + ' service not initialized')
-    }
-    return service
+	assertDependenciesMapExists(dependenciesMap)
+	const service = dependenciesMap[name]
+	if (!service) {
+		throw new Error(name + ' service not initialized')
+	}
+	return service
 }
 
 function assertDependenciesMapExists(depsMap?: DependenciesMap): asserts depsMap is DependenciesMap {
-    if (!depsMap) {
-        throw new Error('Trying getting dependency before initialization')
-    }
+	if (!depsMap) {
+		throw new Error('Trying getting dependency before initialization')
+	}
 }
 
